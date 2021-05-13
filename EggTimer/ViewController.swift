c163d1a683a80b3986de4615e2b460dc2a7f003c
Thomas Strauß <thst@beanfactory.de>
2021-05-14 01:50:57 +0200

    Countdown and play sound, any button resets

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,79 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    
 
+    let TIMER_SPEED = 1.0
 
+    let DONE_TEXT = "Done!"
+
+    let DEFAULT_TEXT = "How do you like your eggs?"
+
+    @IBOutlet weak var timerProgressBar: UIProgressView!
+    @IBOutlet weak var headerLabel: UILabel!
+    var timer: Timer? = nil
+
+    let eggTimes = [
+        "Soft": Float(5.0 * 60.0),
+        "Medium": Float(7.0 * 60.0),
+        "Hard": Float(12.0 * 60.0)
+    ]
+
+//    var eggTime : Float = 1
+
+    @IBAction func onHardnessSelected(_ sender: UIButton) {
+
+        if (headerLabel!.text != DEFAULT_TEXT) {
+            headerLabel.text = DEFAULT_TEXT
+            timerProgressBar.progress = 0.0
+            timer?.invalidate()
+            return
+        }
+
+        let hardness = sender.currentTitle!
+
+        timerProgressBar.progress = 0
+        headerLabel.text = hardness
+
+        var eggTime = eggTimes[hardness] ?? -1.0
+
+        NSLog("Time for %@ is %d", hardness, eggTime)
+
+        if (eggTime == -1) {
+            return
+        }
+
+        if (timer == nil || !(timer?.isValid ?? false)) {
+            runTimer(eggTime: eggTime)
+        } else {
+            stopTimer()
+        }
+    }
+
+    private func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+
+    private func runTimer(eggTime: Float) {
+        var counter: Float = Float(eggTime)
+
+        timer = Timer.scheduledTimer(withTimeInterval: TIMER_SPEED, repeats: true) { [self] (_timer) in
+            counter -= 1;
+            print(counter);
+            timerProgressBar.progress = (eggTime - counter) / eggTime
+            if (counter == 0) {
+                _timer.invalidate()
+                playAlarmClock()
+                headerLabel!.text = "Done!"
+                timerProgressBar.progress = 1
+            }
+        }
+    }
+
+    private func playAlarmClock() {
+        AudioServicesPlayAlertSound(SystemSoundID(1033))
+    }
 }
